da113cb744317d563fdb22ea451058af1702a84a
alex-bailey1 <bail34@pdx.edu>
2021-01-14 12:18:48 -0800

    adding gathering activity

---
+++ gathering_activity/data_gather.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+print("hello")
